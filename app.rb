e32dbc0318fc64925044ca3c82761e8abd069231
Oana Berentan <oana.berentan@yahoo.com>
2019-12-30 01:45:09 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,10 +1,10 @@
-require_relative '../config/environment'
+require_relative '/config/environment'
 
 class App < Sinatra::Base
 
   configure do
     enable :sessions
-    set :session_secret,
+    set :session_secret
    end
 
     get '/' do
